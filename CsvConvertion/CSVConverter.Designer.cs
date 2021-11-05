d8726707c143de65a5803521b3c3a5fa6c63ca43
Van Eester Michiel <michielvaneester@gmail.com>
2021-11-05 07:59:21 +0100

    Initial commit

---
+++ CsvConvertion/CSVConverter.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace CsvConvertion
+{
+    partial class CSVConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Dgv_CsvFile = new System.Windows.Forms.DataGridView();
+            this.Btn_SelectFile = new System.Windows.Forms.Button();
+            this.Txt_File = new System.Windows.Forms.TextBox();
+            this.Btn_AddReeksId = new System.Windows.Forms.Button();
+            this.Rb_Reeks30 = new System.Windows.Forms.RadioButton();
+            this.Rb_Reeks50 = new System.Windows.Forms.RadioButton();
+            this.Rb_Reeks70 = new System.Windows.Forms.RadioButton();
+            this.Rb_Reeks85 = new System.Windows.Forms.RadioButton();
+            this.Rb_Reeks95 = new System.Windows.Forms.RadioButton();
+            this.Rb_Reeks105 = new System.Windows.Forms.RadioButton();
+            this.Rb_Reeks115 = new System.Windows.Forms.RadioButton();
+            this.Btn_ExportCSV = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_CsvFile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Dgv_CsvFile
+            // 
+            this.Dgv_CsvFile.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Dgv_CsvFile.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_CsvFile.Location = new System.Drawing.Point(12, 12);
+            this.Dgv_CsvFile.Name = "Dgv_CsvFile";
+            this.Dgv_CsvFile.Size = new System.Drawing.Size(744, 578);
+            this.Dgv_CsvFile.TabIndex = 0;
+            // 
+            // Btn_SelectFile
+            // 
+            this.Btn_SelectFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Btn_SelectFile.Location = new System.Drawing.Point(775, 12);
+            this.Btn_SelectFile.Name = "Btn_SelectFile";
+            this.Btn_SelectFile.Size = new System.Drawing.Size(109, 23);
+            this.Btn_SelectFile.TabIndex = 1;
+            this.Btn_SelectFile.Text = "Importeer CSV";
+            this.Btn_SelectFile.UseVisualStyleBackColor = true;
+            this.Btn_SelectFile.Click += new System.EventHandler(this.Btn_SelectFile_Click);
+            // 
+            // Txt_File
+            // 
+            this.Txt_File.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Txt_File.Location = new System.Drawing.Point(775, 41);
+            this.Txt_File.Name = "Txt_File";
+            this.Txt_File.Size = new System.Drawing.Size(109, 20);
+            this.Txt_File.TabIndex = 2;
+            // 
+            // Btn_AddReeksId
+            // 
+            this.Btn_AddReeksId.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Btn_AddReeksId.Location = new System.Drawing.Point(775, 130);
+            this.Btn_AddReeksId.Name = "Btn_AddReeksId";
+            this.Btn_AddReeksId.Size = new System.Drawing.Size(109, 38);
+            this.Btn_AddReeksId.TabIndex = 3;
+            this.Btn_AddReeksId.Text = "Wedstrijd nummer toevoegen";
+            this.Btn_AddReeksId.UseVisualStyleBackColor = true;
+            this.Btn_AddReeksId.Click += new System.EventHandler(this.Btn_AddReeksId_Click);
+            // 
+            // Rb_Reeks30
+            // 
+            this.Rb_Reeks30.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Rb_Reeks30.AutoCheck = false;
+            this.Rb_Reeks30.AutoSize = true;
+            this.Rb_Reeks30.Location = new System.Drawing.Point(775, 174);
+            this.Rb_Reeks30.Name = "Rb_Reeks30";
+            this.Rb_Reeks30.Size = new System.Drawing.Size(88, 17);
+            this.Rb_Reeks30.TabIndex = 4;
+            this.Rb_Reeks30.TabStop = true;
+            this.Rb_Reeks30.Text = "Reeks 30 cm";
+            this.Rb_Reeks30.UseVisualStyleBackColor = true;
+            this.Rb_Reeks30.CheckedChanged += new System.EventHandler(this.Rb_Reeks30_CheckedChanged);
+            // 
+            // Rb_Reeks50
+            // 
+            this.Rb_Reeks50.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Rb_Reeks50.AutoCheck = false;
+            this.Rb_Reeks50.AutoSize = true;
+            this.Rb_Reeks50.Location = new System.Drawing.Point(775, 197);
+            this.Rb_Reeks50.Name = "Rb_Reeks50";
+            this.Rb_Reeks50.Size = new System.Drawing.Size(109, 17);
+            this.Rb_Reeks50.TabIndex = 5;
+            this.Rb_Reeks50.TabStop = true;
+            this.Rb_Reeks50.Text = "Reeks 50 - 65 cm";
+            this.Rb_Reeks50.UseVisualStyleBackColor = true;
+            this.Rb_Reeks50.CheckedChanged += new System.EventHandler(this.Rb_Reeks50_CheckedChanged);
+            // 
+            // Rb_Reeks70
+            // 
+            this.Rb_Reeks70.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Rb_Reeks70.AutoCheck = false;
+            this.Rb_Reeks70.AutoSize = true;
+            this.Rb_Reeks70.Location = new System.Drawing.Point(775, 221);
+            this.Rb_Reeks70.Name = "Rb_Reeks70";
+            this.Rb_Reeks70.Size = new System.Drawing.Size(88, 17);
+            this.Rb_Reeks70.TabIndex = 6;
+            this.Rb_Reeks70.TabStop = true;
+            this.Rb_Reeks70.Text = "Reeks 70 cm";
+            this.Rb_Reeks70.UseVisualStyleBackColor = true;
+            this.Rb_Reeks70.CheckedChanged += new System.EventHandler(this.Rb_Reeks70_CheckedChanged);
+            // 
+            // Rb_Reeks85
+            // 
+            this.Rb_Reeks85.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Rb_Reeks85.AutoCheck = false;
+            this.Rb_Reeks85.AutoSize = true;
+            this.Rb_Reeks85.Location = new System.Drawing.Point(775, 245);
+            this.Rb_Reeks85.Name = "Rb_Reeks85";
+            this.Rb_Reeks85.Size = new System.Drawing.Size(88, 17);
+            this.Rb_Reeks85.TabIndex = 7;
+            this.Rb_Reeks85.TabStop = true;
+            this.Rb_Reeks85.Text = "Reeks 85 cm";
+            this.Rb_Reeks85.UseVisualStyleBackColor = true;
+            this.Rb_Reeks85.CheckedChanged += new System.EventHandler(this.Rb_Reeks85_CheckedChanged);
+            // 
+            // Rb_Reeks95
+            // 
+            this.Rb_Reeks95.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Rb_Reeks95.AutoCheck = false;
+            this.Rb_Reeks95.AutoSize = true;
+            this.Rb_Reeks95.Location = new System.Drawing.Point(775, 269);
+            this.Rb_Reeks95.Name = "Rb_Reeks95";
+            this.Rb_Reeks95.Size = new System.Drawing.Size(88, 17);
+            this.Rb_Reeks95.TabIndex = 8;
+            this.Rb_Reeks95.TabStop = true;
+            this.Rb_Reeks95.Text = "Reeks 95 cm";
+            this.Rb_Reeks95.UseVisualStyleBackColor = true;
+            this.Rb_Reeks95.CheckedChanged += new System.EventHandler(this.Rb_Reeks95_CheckedChanged);
+            // 
+            // Rb_Reeks105
+            // 
+            this.Rb_Reeks105.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Rb_Reeks105.AutoCheck = false;
+            this.Rb_Reeks105.AutoSize = true;
+            this.Rb_Reeks105.Location = new System.Drawing.Point(775, 293);
+            this.Rb_Reeks105.Name = "Rb_Reeks105";
+            this.Rb_Reeks105.Size = new System.Drawing.Size(85, 17);
+            this.Rb_Reeks105.TabIndex = 9;
+            this.Rb_Reeks105.TabStop = true;
+            this.Rb_Reeks105.Text = "Reeks 1m05";
+            this.Rb_Reeks105.UseVisualStyleBackColor = true;
+            this.Rb_Reeks105.CheckedChanged += new System.EventHandler(this.Rb_Reeks105_CheckedChanged);
+            // 
+            // Rb_Reeks115
+            // 
+            this.Rb_Reeks115.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Rb_Reeks115.AutoCheck = false;
+            this.Rb_Reeks115.AutoSize = true;
+            this.Rb_Reeks115.Location = new System.Drawing.Point(775, 317);
+            this.Rb_Reeks115.Name = "Rb_Reeks115";
+            this.Rb_Reeks115.Size = new System.Drawing.Size(85, 17);
+            this.Rb_Reeks115.TabIndex = 10;
+            this.Rb_Reeks115.TabStop = true;
+            this.Rb_Reeks115.Text = "Reeks 1m15";
+            this.Rb_Reeks115.UseVisualStyleBackColor = true;
+            this.Rb_Reeks115.CheckedChanged += new System.EventHandler(this.Rb_Reeks115_CheckedChanged);
+            // 
+            // Btn_ExportCSV
+            // 
+            this.Btn_ExportCSV.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Btn_ExportCSV.Location = new System.Drawing.Point(775, 566);
+            this.Btn_ExportCSV.Name = "Btn_ExportCSV";
+            this.Btn_ExportCSV.Size = new System.Drawing.Size(109, 23);
+            this.Btn_ExportCSV.TabIndex = 11;
+            this.Btn_ExportCSV.Text = "Exporteer CSV";
+            this.Btn_ExportCSV.UseVisualStyleBackColor = true;
+            this.Btn_ExportCSV.Click += new System.EventHandler(this.Btn_ExportCSV_Click);
+            // 
+            // CSVConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 602);
+            this.Controls.Add(this.Btn_ExportCSV);
+            this.Controls.Add(this.Rb_Reeks115);
+            this.Controls.Add(this.Rb_Reeks105);
+            this.Controls.Add(this.Rb_Reeks95);
+            this.Controls.Add(this.Rb_Reeks85);
+            this.Controls.Add(this.Rb_Reeks70);
+            this.Controls.Add(this.Rb_Reeks50);
+            this.Controls.Add(this.Rb_Reeks30);
+            this.Controls.Add(this.Btn_AddReeksId);
+            this.Controls.Add(this.Txt_File);
+            this.Controls.Add(this.Btn_SelectFile);
+            this.Controls.Add(this.Dgv_CsvFile);
+            this.Name = "CSVConverter";
+            this.Text = "Edit and Convert csv File";
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_CsvFile)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Dgv_CsvFile;
+        private System.Windows.Forms.Button Btn_SelectFile;
+        private System.Windows.Forms.TextBox Txt_File;
+        private System.Windows.Forms.Button Btn_AddReeksId;
+        private System.Windows.Forms.RadioButton Rb_Reeks30;
+        private System.Windows.Forms.RadioButton Rb_Reeks50;
+        private System.Windows.Forms.RadioButton Rb_Reeks70;
+        private System.Windows.Forms.RadioButton Rb_Reeks85;
+        private System.Windows.Forms.RadioButton Rb_Reeks95;
+        private System.Windows.Forms.RadioButton Rb_Reeks105;
+        private System.Windows.Forms.RadioButton Rb_Reeks115;
+        private System.Windows.Forms.Button Btn_ExportCSV;
+    }
+}
+
---
+++ CsvConvertion/CSVConverter.cs
@@ -0,0 +1,313 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsvConvertion
+{
+    public partial class CSVConverter : Form
+    {
+        public int SelectedReeks = 0;
+        public bool ReeksIdAdded = false;
+        public string ExportPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Output.csv");
+        public CSVConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_SelectFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.ShowDialog();
+                string SourceURL = "";
+
+                if (dialog.FileName != "")
+                {
+                    if (dialog.FileName.EndsWith(".csv"))
+                    {
+                        DataTable dtNew = new DataTable();
+                        dtNew = GetDataTableFromCsvFile(dialog.FileName);
+                        
+                        Txt_File.Text = dialog.SafeFileName;
+                        SourceURL = dialog.FileName;
+
+                        if (dtNew.Rows != null && dtNew.Rows.ToString() != String.Empty)
+                        {
+                            Dgv_CsvFile.DataSource = dtNew;
+                        }
+                        
+                        if (Dgv_CsvFile.Rows.Count == 0)
+                        {
+                            MessageBox.Show("Geen data in csv Bestand", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    } 
+                    else
+                    {
+                        MessageBox.Show("Gekozen bestand is ongeldig. Kies een csv Bestand.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}", "Error");
+            }
+        }
+
+        public static DataTable GetDataTableFromCsvFile(string fileName)
+        {
+            DataTable csvData = new DataTable();
+
+            try
+            {
+                if (fileName.EndsWith("csv"))
+                {
+                    using (TextFieldParser csvReader = new TextFieldParser(fileName))
+                    {
+                        csvReader.SetDelimiters(new string[] { "," });
+                        csvReader.HasFieldsEnclosedInQuotes = true;
+
+                        //read Column
+                        string[] colFields = csvReader.ReadFields();
+                        foreach (string column in colFields)
+                        {
+                            //'Naam ruiter' split into 'Voornaam' en 'Achternaam'
+                            if (column == "Naam ruiter")                //Full name
+                            {
+                                csvData.Columns.Add("Voornaam");        //First name
+                                csvData.Columns.Add("Achternaam");      //Last name
+                            }
+                            else
+                            {
+                                DataColumn datacolumn = new DataColumn(column);
+                                datacolumn.AllowDBNull = true;
+
+                                csvData.Columns.Add(datacolumn);
+                            }
+                        }
+
+                        while (!csvReader.EndOfData)
+                        {
+                            string[] fieldData = csvReader.ReadFields();
+                            for (int i = 0; i < fieldData.Length - 1; i++)
+                            {
+                                if (fieldData[i] == "")
+                                {
+                                    fieldData[i] = null;
+                                }
+                            }
+
+                            //Split: fieldData[0] -> split op 1ste spatie
+                            string[] splitted = fieldData[0].Split(new[] { ' ' }, 2);
+
+                            Array.Resize(ref fieldData, 5);
+
+                            fieldData[4] = fieldData[3];
+                            fieldData[3] = fieldData[2];
+                            fieldData[2] = fieldData[1];
+                            fieldData[1] = splitted[1];
+                            fieldData[0] = splitted[0];
+
+                            csvData.Rows.Add(fieldData);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}", "Error");
+            }
+            return csvData;
+        }
+
+        private void Rb_Reeks30_CheckedChanged(object sender, EventArgs e)
+        {
+            SelectedReeks = 1;
+        }
+
+        private void Rb_Reeks50_CheckedChanged(object sender, EventArgs e)
+        {
+            SelectedReeks = 2;
+        }
+
+        private void Rb_Reeks70_CheckedChanged(object sender, EventArgs e)
+        {
+            SelectedReeks = 3;
+        }
+
+        private void Rb_Reeks85_CheckedChanged(object sender, EventArgs e)
+        {
+            SelectedReeks = 4;
+        }
+
+        private void Rb_Reeks95_CheckedChanged(object sender, EventArgs e)
+        {
+            SelectedReeks = 5;
+        }
+
+        private void Rb_Reeks105_CheckedChanged(object sender, EventArgs e)
+        {
+            SelectedReeks = 6;
+        }
+
+        private void Rb_Reeks115_CheckedChanged(object sender, EventArgs e)
+        {
+            SelectedReeks = 7;
+        }
+
+        private void Btn_AddReeksId_Click(object sender, EventArgs e)
+        {
+            if (Dgv_CsvFile.Rows.Count != 0)
+            {
+                if (SelectedReeks != 0)
+                {
+                    Dgv_CsvFile.Columns.Add("Wedstrijd nummer", "Wedstrijd nummer");
+
+                    for (int i = 0; i < Dgv_CsvFile.Rows.Count - 1; i++)
+                    {
+                        Dgv_CsvFile.Rows[i].Cells[5].Value = SelectedReeks;
+                    }
+
+                    AdjustColumnOrder();
+                    ReeksIdAdded = true;
+                    Btn_AddReeksId.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("Kies een Reeks.", "Warning");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Importeer eerst een csv Bestand.", "Warning");
+            }
+        }
+
+        private void Btn_ExportCSV_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Dgv_CsvFile.Rows.Count != 0)
+                {
+                    if (ReeksIdAdded)
+                    {
+                        try
+                        {
+                            //Put Columns in order of display
+                            List<DataGridViewColumn> columnsInOrder = new List<DataGridViewColumn>();
+
+                            foreach (DataGridViewColumn c in Dgv_CsvFile.Columns)
+                            {
+                                columnsInOrder.Add(c);
+                            }
+
+                            columnsInOrder = columnsInOrder.OrderBy(c => c.DisplayIndex).ToList();
+
+                            //Read Rows into lines of string to export to csv
+                            List<string> CsvExportLines = new List<string>();
+
+                            foreach (DataGridViewRow row in Dgv_CsvFile.Rows)
+                            {
+                                List<string> valuesInOrder = new List<string>();
+
+                                foreach (DataGridViewColumn col in columnsInOrder)
+                                {
+                                    if (!row.IsNewRow)
+                                    {
+                                        valuesInOrder.Add(row.Cells[col.Index].Value.ToString());
+                                    }
+                                }
+
+                                CsvExportLines.Add(string.Join(";", valuesInOrder.ToArray()));
+                            }
+
+                            StreamWriter Output = File.AppendText(ExportPath);
+
+                            try
+                            {
+                                for (int i = 0; i <= CsvExportLines.Count - 2; i++)
+                                {
+                                    Output.WriteLine(CsvExportLines[i]);
+                                }
+
+                                //Clear DataGridView
+                                ClearWinForm();
+
+                                MessageBox.Show("Data is succesvol geëxporteerd naar de output.csv", "Succes");
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show($"{ex}", "Error");
+                            }
+                            finally
+                            {
+                                Output.Close();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"{ex}", "Error");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Voeg eerst de wedstrijd nummer toe.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Importeer eerst een csv Bestand.", "Warning");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}", "Error");
+            }
+        }
+
+        private void AdjustColumnOrder()
+        {
+            //Change order
+            Dgv_CsvFile.Columns["Voornaam"].DisplayIndex = 0;           //Display 0     Real 0
+            Dgv_CsvFile.Columns["Achternaam"].DisplayIndex = 1;         //Display 1     Real 1
+            Dgv_CsvFile.Columns["Naam paard"].DisplayIndex = 2;         //Display 2     Real 2
+            Dgv_CsvFile.Columns["Wedstrijd nummer"].DisplayIndex = 3;   //Display 3     Real 5
+            Dgv_CsvFile.Columns["Volgorde"].DisplayIndex = 4;           //Display 4     Real 4
+            Dgv_CsvFile.Columns["Type"].DisplayIndex = 5;               //Display 5     Real 3
+
+            //Change header value
+            Dgv_CsvFile.Columns[4].HeaderCell.Value = "Startnummer";
+        }
+
+        private void ClearWinForm()
+        {
+            //Clear DataGridView
+            Dgv_CsvFile.DataSource = null;
+            Dgv_CsvFile.Columns.Remove("Wedstrijd nummer");
+
+            //Clear UI
+            Txt_File.Text = null;
+            Btn_AddReeksId.Enabled = true;
+
+            //Uncheck All
+            Rb_Reeks30.Checked = false;
+            Rb_Reeks50.Checked = false;
+            Rb_Reeks70.Checked = false;
+            Rb_Reeks85.Checked = false;
+            Rb_Reeks95.Checked = false;
+            Rb_Reeks105.Checked = false;
+            Rb_Reeks115.Checked = false;
+
+            SelectedReeks = 0;
+            ReeksIdAdded = false;
+        }
+    }
+}
